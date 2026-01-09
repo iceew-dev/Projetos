979c45640b48aa5c3b2dfbb011921e8597ad5b04
Caio Victor <caiovictor1107@gmail.com>
2026-01-09 17:17:31 -0300

    FirstCommit

---
+++ cursoweb/src/test/java/com/caiodev/cursoweb/CursowebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiodev.cursoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursowebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
